a23e992f19495ad1c99dd19e6e38e378a8cb8dd5
Matt Condon <mattgcondon@gmail.com>
2015-08-18 20:54:37 -0500

    mostly working, I guess

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # EMP
 more like NO-tifications
+
+Plan:
+- when someone clicks into a comment box, add a action item to
+`.UFICommentAttachmentButtons` with an `ajaxify` attribute to toggle notifications for that post
+- get the post ID by going from the comment box up to the parent
+`[id*="mall_post"]` and then getting the post id from `mall_post_<id>`
+- use that post ID in the ajaxify element
+- set up a keycommand `ctrl-shift-enter` to submit _and_ turn off notifications
+- the EMP icon fades in and out in the new comment box after you ctrl-shift enter
+- 
--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,3 +1,39 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+function disableKeypressListener() {
+  key.unbind('ctrl+shift+enter');
+}
+
+function enableKeypressListener(postId) {
+  key('ctrl+shift+enter', function() {
+    // var ajaxifyElement = $('<a/>', {
+    //   rel: 'async-post',
+    //   ajaxify: '/ajax/litestand/follow_post?message_id=' + postId + '&follow=0'
+    // });
+
+    var ajaxifyElement = $('<a href="#" rel="async-post" ajaxify="/ajax/litestand/follow_post?message_id=883063921763607&amp;follow=1" role="menuitem"><span><span class="_54nh">Turn on notifications</span></span></a>');
+
+    $('body').append(ajaxifyElement);
+    ajaxifyElement.trigger('click');
+    // ajaxifyElement.remove();
+    console.log('clicked');
+
+    disableKeypressListener();
+  });
+}
+
+$('body').on('click', function(e) {
+  var target = $(e.target);
+  if (!target.parents('.UFICommentContainer').length) {
+    return;
+  }
+
+  var postId = target.parents('[id*="mall_post"]').attr('id').replace('mall_post_', '');
+
+  enableKeypressListener(postId);
+
+  $(e.target).find('[role="combobox"]').on('blur', function() {
+    // remove keypress listener
+    disableKeypressListener();
+  });
+});
