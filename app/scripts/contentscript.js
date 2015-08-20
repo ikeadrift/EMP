52065ec0e883bd957f28b91c8c0a05254f5c38af
Benjamin Zweig <benzilla@me.com>
2015-08-20 04:37:15 -0700

    garbage icon fadein

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -56,7 +56,9 @@ $(document).ready(function() {
           type: 'AsyncRequest',
           endpoint: '/ajax/litestand/follow_post?follow=0&message_id=' + request.postId
         }, window.location.origin);
-        $('#EMP').parents('.UFIInputContainer').find('.UFICommentAttachmentButtons').append('<div style="width: 14px; height: 14px; margin-top: 9px; margin-left: -24px;margin-bottom:4px; background-image:url("chrome.extension.getURL("images/comment-icon.png")");background-size: contain;"></div>').fadeIn().next().delay(500).fadeOut();
+        var EMPicon = chrome.extension.getURL('images/comment-icon.png');
+        console.log(EMPicon);
+        $('#EMP').parents('.UFIInputContainer').find('.UFICommentAttachmentButtons').append($('<div id="EMPicon"></div>').css({"width":"14px","height":"14px","margin-top":"9px","margin-left":"-24px","background-image":"url('"+EMPicon+"')","background-size":"contain"})).fadeIn().next().delay(500).fadeOut();
 
         disableKeypressListener();
       }
